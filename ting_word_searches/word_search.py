5b822d492bb48483a63167632733da73c6de3940
Mathews <mathewsrodrigues9@gmail.com>
2023-04-04 19:51:07 -0300

    criando funcao exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for item in instance._data:
+        ocorrencias = list()
+        for count, row in enumerate(item["linhas_do_arquivo"], start=1):
+            if word.lower() in row.lower():
+                ocorrencias.append({"linha": count})
+
+        if len(ocorrencias) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": item["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
