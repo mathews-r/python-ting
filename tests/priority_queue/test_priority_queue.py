0e0d5d758fbc5a1dc24322c7d85d77b05cb9a974
Mathews <mathewsrodrigues9@gmail.com>
2023-04-04 18:37:33 -0300

    testando a função priority_queueing

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,45 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    priority = {
+        "nome_do_arquivo": "teste.txt",
+        "qtd_linhas": 3,
+        "linhas_do_arquivo": ["linha1", "linha2", "linha3"],
+    }
+
+    not_priority = {
+        "nome_do_arquivo": "teste.txt",
+        "qtd_linhas": 6,
+        "linhas_do_arquivo": [
+            "linha1",
+            "linha2",
+            "linha3",
+            "linha4",
+            "linha5",
+            "linha6",
+        ],
+    }
+
+    queue.enqueue(not_priority)
+    queue.enqueue(priority)
+
+    assert len(queue) == 2
+
+    item = queue.search(0)
+    assert item["qtd_linhas"] == 3
+
+    item2 = queue.search(1)
+    assert item2["qtd_linhas"] == 6
+
+    queue.dequeue()
+    assert len(queue) == 1
+
+    item3 = queue.search(0)
+    assert item3["qtd_linhas"] == 6
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(50)
