621a175e9e2f2de3a83119f1d5e84e453a24fc01
Mathews <mathewsrodrigues9@gmail.com>
2023-04-04 16:50:49 -0300

    criando função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for path in instance._data:
+        if path_file in path["nome_do_arquivo"]:
+            return None
+
+    dict_path = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+
+    instance.enqueue(dict_path)
+    print(dict_path, file=sys.stdout)
 
 
 def remove(instance):
