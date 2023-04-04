b6446bc39466266a080c8a260ba66d50916b1503
Mathews <mathewsrodrigues9@gmail.com>
2023-04-04 15:20:35 -0300

    desenvolvendo classe Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,25 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.size() == 0:
+            return self._data.pop(0)
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        try:
+            return self._data[index]
+        except IndexError:
+            raise IndexError("Índice Inválido ou Inexistente")
+
+    def size(self):
+        return len(self._data)
